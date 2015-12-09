8b27b87af98e4da4a9ab6c4e807561282dfe2fff
Roman Kushnarenko <sromku@gmail.com>
2015-12-09 09:55:43 +0200

    Small cleans

--- generator.go
+++ generator.go
@@ -4,20 +4,20 @@ import (
 	"encoding/json"
 	"fmt"
 	"html/template"
+	"io"
 	"io/ioutil"
+	"log"
 	"os"
 	"time"
-	"io"
-	"log"
 )
 
 func main() {
 
 	// check current parent folder
-	dir, _ := os.Getwd()
-	fmt.Println("Current dir: " + dir);
+	// dir, _ := os.Getwd()
+	// fmt.Println("Current dir: " + dir)
 
-	fmt.Println("-- Preparing and moving api.json and responses files ...")
+	fmt.Println("* Preparing and moving api.json and responses files ...")
 
 	if os.Args == nil || len(os.Args) == 1 {
 		fmt.Println("ERROR: You must provide path to api.json")
@@ -37,21 +37,21 @@ func main() {
 
 	// clean input dir
 	os.RemoveAll(relativePath + "input")
-	os.Mkdir(relativePath + "input", os.ModePerm)
+	os.Mkdir(relativePath+"input", os.ModePerm)
 
 	// copy api.json
-	CopyFile(apiPath + "/api.json", relativePath + "input/api.json")
+	CopyFile(apiPath+"/api.json", relativePath+"input/api.json")
 
 	// copy responses if exist
-	err := CopyDir(apiPath + "/responses", relativePath + "input/responses")
+	err := CopyDir(apiPath+"/responses", relativePath+"input/responses")
 	if err != nil {
 		log.Fatal(err)
 	} else {
 		log.Print("Files copied.")
 	}
 
-	fmt.Println("-- start api docs generation...")
-	fmt.Println("-- read input: api.json")
+	fmt.Println("* Start api docs generation...")
+	fmt.Println("* Read input: api.json")
 
 	file, e := ioutil.ReadFile(relativePath + "input/api.json")
 	if e != nil {
@@ -59,11 +59,20 @@ func main() {
 		os.Exit(1)
 	}
 	var api API
-	json.Unmarshal(file, &api)
+	err = json.Unmarshal(file, &api)
+	if err != nil {
+		panic(err)
+	}
 
-	fmt.Println("-- load resources")
+	fmt.Printf("* Load resources (%v)", len(api.Endpoints))
+	fmt.Println("")
 	for i, endpoint := range api.Endpoints {
-		response, e := ioutil.ReadFile(relativePath + "input/responses/" + endpoint.Response)
+
+		fmt.Printf("::: %v", endpoint.Response)
+
+		resource := relativePath + "input/responses/" + endpoint.Response
+		fmt.Printf("* Resource: %v", resource)
+		response, e := ioutil.ReadFile(resource)
 		if e != nil {
 			fmt.Printf("ERROR: File error: %v\n", e)
 			os.Exit(1)
@@ -84,7 +93,7 @@ func main() {
 		}
 	}
 
-	fmt.Println("-- create doc")
+	fmt.Println("* Create doc")
 	doc := &Doc{
 		Title:       api.Title,
 		GeneratedAt: time.Now().Format(time.RFC1123),
@@ -94,7 +103,7 @@ func main() {
 	f, _ := os.Create(relativePath + "theme/doc.html")
 	docTemplate.ExecuteTemplate(f, "doc", doc)
 
-	fmt.Println("-- finish generation")
+	fmt.Println("* Finish docs generation")
 }
 
 func createCurlExample(baseUrl string, endpoint *Endpoint) string {
@@ -102,91 +111,7 @@ func createCurlExample(baseUrl string, endpoint *Endpoint) string {
 	return example
 }
 
-
-func CopyFile(source string, dest string) (err error) {
-	sf, err := os.Open(source)
-	if err != nil {
-		return err
-	}
-	defer sf.Close()
-	df, err := os.Create(dest)
-	if err != nil {
-		return err
-	}
-	defer df.Close()
-	_, err = io.Copy(df, sf)
-	if err == nil {
-		si, err := os.Stat(source)
-		if err != nil {
-			err = os.Chmod(dest, si.Mode())
-		}
-
-	}
-
-	return
-}
-
-// Recursively copies a directory tree, attempting to preserve permissions.
-// Source directory must exist, destination directory must *not* exist.
-func CopyDir(source string, dest string) (err error) {
-
-	// get properties of source dir
-	fi, err := os.Stat(source)
-	if err != nil {
-		return err
-	}
-
-	if !fi.IsDir() {
-		return &CustomError{"Source is not a directory"}
-	}
-
-	// ensure dest dir does not already exist
-
-	_, err = os.Open(dest)
-	if !os.IsNotExist(err) {
-		return &CustomError{"Destination already exists"}
-	}
-
-	// create dest dir
-
-	err = os.MkdirAll(dest, fi.Mode())
-	if err != nil {
-		return err
-	}
-
-	entries, err := ioutil.ReadDir(source)
-
-	for _, entry := range entries {
-
-		sfp := source + "/" + entry.Name()
-		dfp := dest + "/" + entry.Name()
-		if entry.IsDir() {
-			err = CopyDir(sfp, dfp)
-			if err != nil {
-				log.Println(err)
-			}
-		} else {
-			// perform copy
-			err = CopyFile(sfp, dfp)
-			if err != nil {
-				log.Println(err)
-			}
-		}
-
-	}
-	return
-}
-
-// A struct for returning custom error messages
-type CustomError struct {
-	What string
-}
-
-// Returns the error message defined in What as a string
-func (e *CustomError) Error() string {
-	return e.What
-}
-
+// model
 type Doc struct {
 	Title       string
 	GeneratedAt string
@@ -194,7 +119,7 @@ type Doc struct {
 }
 
 type API struct {
-	Title 	  string 	 `json:"title"`
+	Title     string     `json:"title"`
 	Base      Base       `json:"base"`
 	Endpoints []Endpoint `json:"endpoints"`
 }
@@ -237,4 +162,85 @@ type ResultCode struct {
 	Code        int    `json:"code"`
 	Type        string `json:"type"`
 	Description string `json:"description"`
+}
+
+// utils
+func CopyFile(source string, dest string) (err error) {
+	sf, err := os.Open(source)
+	if err != nil {
+		return err
+	}
+	defer sf.Close()
+	df, err := os.Create(dest)
+	if err != nil {
+		return err
+	}
+	defer df.Close()
+	_, err = io.Copy(df, sf)
+	if err == nil {
+		si, err := os.Stat(source)
+		if err != nil {
+			err = os.Chmod(dest, si.Mode())
+		}
+
+	}
+
+	return
+}
+
+func CopyDir(source string, dest string) (err error) {
+
+	// get properties of source dir
+	fi, err := os.Stat(source)
+	if err != nil {
+		return err
+	}
+
+	if !fi.IsDir() {
+		return &IOError{"Source is not a directory"}
+	}
+
+	// ensure dest dir does not already exist
+
+	_, err = os.Open(dest)
+	if !os.IsNotExist(err) {
+		return &IOError{"Destination already exists"}
+	}
+
+	// create dest dir
+
+	err = os.MkdirAll(dest, fi.Mode())
+	if err != nil {
+		return err
+	}
+
+	entries, err := ioutil.ReadDir(source)
+
+	for _, entry := range entries {
+
+		sfp := source + "/" + entry.Name()
+		dfp := dest + "/" + entry.Name()
+		if entry.IsDir() {
+			err = CopyDir(sfp, dfp)
+			if err != nil {
+				log.Println(err)
+			}
+		} else {
+			// perform copy
+			err = CopyFile(sfp, dfp)
+			if err != nil {
+				log.Println(err)
+			}
+		}
+
+	}
+	return
+}
+
+type IOError struct {
+	What string
+}
+
+func (e *IOError) Error() string {
+	return e.What
 }
